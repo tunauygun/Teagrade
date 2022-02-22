fa7f5b67f16f8f076fa7f1333235d4279db2ce96
Tuna Uygun <uyguntuna2018@gmail.com>
2022-02-22 18:30:10 -0500

    Add session and flash

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override')
 const Course = require('./models/course');
 const ExpressError = require('./utils/ExpressError');
-
+const session = require('express-session');
+const flash = require('connect-flash');
 const courses = require('./routes/courses');
-
 
 mongoose.connect('mongodb://localhost:27017/teagrade');
 
@@ -25,6 +25,26 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'temporarySecretForTesting',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge: 1000*60*60*24*7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use('/courses', courses)
 
--- routes/courses.js
+++ routes/courses.js
@@ -28,22 +28,32 @@ router.get('/new', (req, res) => {
 router.post('/', validateCourse, catchAsync(async (req, res) => {
     const course = new Course(req.body.course);
     await course.save();
+    req.flash('success', 'Successfully created a new course!');
     res.redirect(`/courses/${course._id}`)
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
     const course = await Course.findById(req.params.id);
+    if(!course){
+        req.flash('error', 'Cannot find the requested course!');
+        return res.redirect('/courses');
+    }
     res.render('courses/show', { course });
 }))
 
 router.get('/:id/edit', async (req, res) => {
     const course = await Course.findById(req.params.id);
+    if(!course){
+        req.flash('error', 'Cannot find the requested course!');
+        return res.redirect('/courses');
+    }
     res.render('courses/edit', { course });
 })
 
 router.put('/:id', validateCourse, async (req, res) => {
     const { id } = req.params;
     const course = await Course.findByIdAndUpdate(id, { ...req.body.course })
+    req.flash('success', 'Successfully updated course!');
     res.redirect(`/courses/${course._id}`);
 
 })
