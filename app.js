07a0ab950b280dd3c71131533d6a9f2a221803cb
Tuna Uygun <uyguntuna2018@gmail.com>
2023-01-02 15:32:24 -0500

    Added Mongoose Sanitize

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const userRoutes = require('./routes/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 mongoose.connect('mongodb://localhost:27017/teagrade');
 
@@ -36,6 +37,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: process.env.SECRET,
